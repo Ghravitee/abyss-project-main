6442f31ac06271c021debf2621d6205b10ca2933
Ayodele Temitope <dyenamics228@gmail.com>
2025-03-29 18:53:11 +0100

    remove errors from typescript

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -13,12 +13,15 @@ function LoadingScreen() {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black z-50">
-      {/* <button onClick={() => setLoading(!loading)}>Toggle Loader</button>
+      <button onClick={() => setLoading(!loading)} className="hidden">
+        Toggle Loader
+      </button>
       <input
         value={color}
         onChange={(input) => setColor(input.target.value)}
         placeholder="Color of the loader"
-      /> */}
+        className="hidden"
+      />
 
       <div className="flex flex-col items-center justify-center ">
         <CircleLoader
